dd4b7ef1ff8dbea49f128c8c778d2c58ba2a4f55
EDward1101-bit <moelst11@gmail.com>
2025-12-12 17:58:18 +0200

    Add project files.

---
+++ OnlineShopProject_dNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineShopProject_dNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
