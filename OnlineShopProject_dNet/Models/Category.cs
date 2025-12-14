5044ba818a1304fd46df33200e71dc8b68a2f9f4
Chris <149780155+NovaCX23@users.noreply.github.com>
2025-12-14 18:29:15 +0200

    Basic class implementation for product, category, review

---
+++ OnlineShopProject_dNet/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopProject_dNet.Models
+{
+    public class Category
+    {
+        // Atribute pentru validare
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Category name required")]
+        public string Name { get; set; }
+        
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+}
