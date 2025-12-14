e1e5e93188c61f70d2f4962c27578cbbe50d2745
Chris <149780155+NovaCX23@users.noreply.github.com>
2025-12-14 18:39:28 +0200

    prop in context bd pt migratii

--- OnlineShopProject_dNet/Data/ApplicationDbContext.cs
+++ OnlineShopProject_dNet/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OnlineShopProject_dNet.Models;
 
 namespace OnlineShopProject_dNet.Data
 {
@@ -9,5 +10,8 @@ namespace OnlineShopProject_dNet.Data
             : base(options)
         {
         }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
     }
 }
