e304dab6cfa0cafcdbf7672c49c90ab3c126b33d
EDward1101-bit <moelst11@gmail.com>
2025-12-23 12:18:10 +0200

    adaugare migrare plus TODO la edit.cshtm -PRODUCTS, LINIA 12

---
+++ OnlineShopProject_dNet/Data/Migrations/20251223101630_addCategUnica.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineShopProject_dNet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addCategUnica : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
